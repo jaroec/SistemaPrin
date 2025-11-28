4e302ff7e693a92b5ab429f0c43442db55ba420f
jaroec <96500713+jaroec@users.noreply.github.com>
2025-11-28 15:41:13 -0400

    Update __init__.py

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,8 +1,9 @@
+# backend/app/db/models/__init__.py
 from app.db.models.user import User
 from app.db.models.client import Client
 from app.db.models.product import Product
+from app.db.models.sale import Sale
 from app.db.models.sale_detail import SaleDetail
 from app.db.models.payment import Payment
-from app.db.schemas.pos import PaymentMethodEnum
 
-__all__ = ["User", "Client", "Product", "SaleDetail", "Payment", "PaymentMethodEnum" ]
+__all__ = ["User", "Client", "Product", "Sale", "SaleDetail", "Payment"]
