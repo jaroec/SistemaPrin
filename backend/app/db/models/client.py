1fc9353642fd5a72441e48d7fff712b987ccc207
jaroec <96500713+jaroec@users.noreply.github.com>
2025-12-17 15:12:16 -0400

    Update client.py

--- backend/app/db/models/client.py
+++ backend/app/db/models/client.py
@@ -17,3 +17,4 @@ class Client(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
