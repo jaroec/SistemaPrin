26a2dac5291db147aa8e438d3bb275c0946d2b6f
jaroec <96500713+jaroec@users.noreply.github.com>
2025-12-20 14:09:03 -0400

    Delete backend/app/db/models directory

--- backend/app/db/models/expense_enums.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-class ExpenseCategory(enum.Enum):
-    PROVEEDORES = "PROVEEDORES"
-    NOMINA = "NOMINA"
-    SERVICIOS = "SERVICIOS"
-    COMPRAS = "COMPRAS"
-    ADMINISTRATIVO = "ADMINISTRATIVO"
-    OTROS = "OTROS"
